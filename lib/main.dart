4f2e31c731680b665a95a994fc9a26ca40f5fd59
Snoy Kuo <snoy.kuo@twprotech.com>
2021-04-27 15:01:51 +0800

    add macOS support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 // @dart=2.9
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 import 'app.dart';
 import 'common/repository/remote_repository.dart';
@@ -7,6 +10,11 @@ import 'common/repository/remote_repository.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Pokedex Provider');
+    setWindowMinSize(const Size(375, 667)); //iPhone 1x
+    setWindowMaxSize(Size.infinite);
+  }
   runApp(App(
     repository: RemoteRepository(),
   ));
